1cbe7b9ed93f5939efde9e65acf85b7c373392a7
ANNA <ANNA@PORTABLE.mshome.net>
2012-08-04 11:25:15 +0200

    Add the forget sign

--- src/com/ptibiscuit/iprofession/listeners/LearnManagerSign.java
+++ src/com/ptibiscuit/iprofession/listeners/LearnManagerSign.java
@@ -2,9 +2,11 @@ package com.ptibiscuit.iprofession.listeners;
 
 import com.ptibiscuit.iprofession.Plugin;
 import com.ptibiscuit.iprofession.data.models.Profession;
+import java.util.ArrayList;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -15,6 +17,8 @@ public class LearnManagerSign implements Listener {
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerInteract(PlayerInteractEvent e)
 	{
+		Plugin plug = Plugin.getInstance();
+		Player player = e.getPlayer();
 		Block b = e.getClickedBlock();
 		if (b != null && (b.getType() == Material.SIGN_POST || b.getType() == Material.WALL_SIGN))
 		{
@@ -22,22 +26,47 @@ public class LearnManagerSign implements Listener {
 			if (sign.getLine(0).contains("[iProfessions]"))
 			{
 				String[] data = sign.getLine(1).split(" ");
+				// On ajoute des arguments qui pourrait etre sur la deuxième ligne
 				if (data[0].equalsIgnoreCase("learn"))
 				{
 					Profession p = Plugin.getData().getProfession(data[1]);
 					if (p != null)
 					{
-						if (!Plugin.getInstance().getPermissionHandler().has(e.getPlayer(), "sign.learn." + p.getTag(), true))
+						if (!plug.getPermissionHandler().has(player, "sign.learn." + p.getTag(), true)
+								  && !plug.getPermissionHandler().has(player, "sign.learn", true))
 						{
-							Plugin.getInstance().sendPreMessage(e.getPlayer(), "have_perm");
+							plug.sendPreMessage(player, "have_perm");
 							e.setCancelled(true);
 							return;
 						}
-						Plugin.getInstance().tryToLearn(e.getPlayer(), p);
+						plug.tryToLearn(player, p);
 					}
 					else
 					{
-						Plugin.getInstance().sendPreMessage(e.getPlayer(), "unknown_tag");
+						plug.sendPreMessage(player, "unknown_tag");
+					}
+				} else if (data[0].equalsIgnoreCase("forget")) {
+					Profession p = Plugin.getData().getProfession(data[1]);
+					if (p != null)
+					{
+						if (!plug.getPermissionHandler().has(player, "sign.forget." + p.getTag(), true)
+								  && !plug.getPermissionHandler().has(player, "sign.forget", true))
+						{
+							Plugin.getInstance().sendPreMessage(player, "have_perm");
+							e.setCancelled(true);
+							return;
+						}
+						ArrayList<Profession> playerProfs = Plugin.getData().getProfessionByPlayer(player.getName());
+						if (playerProfs.contains(p)) {
+							plug.removeProfessionToPlayer(p, player);
+							plug.sendPreMessage(player, "forget_succ");
+						} else {
+							plug.sendMessage(player, plug.getSentence("havnt_profession"));
+						}
+					}
+					else
+					{
+						Plugin.getInstance().sendPreMessage(player, "unknown_tag");
 					}
 				}
 			}

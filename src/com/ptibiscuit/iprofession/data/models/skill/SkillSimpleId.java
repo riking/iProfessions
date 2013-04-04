3717b4c26b3c75a55e935c28007d2222ef1496a0
riking <rikingcoding@gmail.com>
2013-04-03 23:24:11 -0700

    Change SkillSimpleId to a HashMap

--- src/com/ptibiscuit/iprofession/data/models/skill/SkillSimpleId.java
+++ src/com/ptibiscuit/iprofession/data/models/skill/SkillSimpleId.java
@@ -1,54 +1,42 @@
-/*
- * To change this template, choose Tools | Templates and open the template in
- * the editor.
- */
 package com.ptibiscuit.iprofession.data.models.skill;
 
-import java.util.AbstractMap.SimpleEntry;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-
 
 /**
- *
- * @author ANNA
+ * A base class that holds a collection of block / item IDs with damage
+ * values.
  */
 public class SkillSimpleId extends Skill {
-    private ArrayList<Entry<Integer, Integer>> ids = new ArrayList<Entry<Integer, Integer>>();
+    private Map<Integer, Integer> ids = new HashMap<Integer, Integer>();
 
     @Override
     public void onEnable(Map<?, ?> config) {
+        /// XXX Should be YAML parse
         for (String idString : config.get("id").toString().split(",")) {
             String[] dataIdString = idString.split("-");
             if (dataIdString.length == 1) {
-                this.ids.add(new SimpleEntry(new Integer(dataIdString[0]), -1));
+                // New wildcard is Short.MAX_VALUE, not -1
+                this.ids.put(Integer.valueOf(dataIdString[0]), Integer.valueOf(Short.MAX_VALUE));
             } else {
-                this.ids.add(new SimpleEntry(new Integer(dataIdString[0]), new Integer(dataIdString[1])));
+                this.ids.put(Integer.valueOf(dataIdString[0]), Integer.valueOf(dataIdString[1]));
             }
         }
     }
 
-    public ArrayList<Entry<Integer, Integer>> getIds() {
+    public Map<Integer, Integer> getIds() {
         return ids;
     }
 
     public boolean hasId(int id, int meta_data) {
-        for (Entry<Integer, Integer> idFocus : this.ids) {
-            if (idFocus.getKey() == id) {
-                if (meta_data == idFocus.getValue() || idFocus.getValue() == -1)
-                    return true;
-            }
+        if (ids.containsKey(id)) {
+            Integer val = ids.get(Integer.valueOf(id));
+            return (val == meta_data) || (val == Short.MAX_VALUE);
         }
-
         return false;
     }
 
     public boolean containsId(int id) {
-        for (Entry<Integer, Integer> idFoc : this.ids) {
-            if (idFoc.getKey() == id)
-                return true;
-        }
-        return false;
+        return ids.containsKey(Integer.valueOf(id));
     }
 }
